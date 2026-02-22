c360d62fc6998c5169b7055cc98f8f0caa5bc8ff
Igland <igland@gmail.com>
2026-02-23 00:27:29 +0100

    Add PREMIRROR for fuse to bypass 404

--- update_local_conf.py
+++ update_local_conf.py
@@ -12,23 +12,22 @@ with open(conf_path, 'r') as f:
 new_lines = []
 for line in lines:
     # Remove any previous entries related to my fixes
-    if '# Direct fetch from GitHub' in line or \
-       'SQUEEZEOS_SVN =' in line or \
-       'SQUEEZEPLAY_SCM =' in line or \
+    if '# SqueezeOS Build Patch Overrides' in line or \
+       'DL_DIR =' in line or \
        'GIT =' in line or \
-       '# Fix git fetch' in line or \
-       'DL_DIR =' in line:
+       'PREMIRRORS' in line or \
+       'http://downloads.sourceforge.net/fuse/fuse-2.7.2.tar.gz' in line:
         continue
     new_lines.append(line)
 
-# Add robust overrides
+# Add definitive overrides
 new_lines.append('\n# SqueezeOS Build Patch Overrides\n')
-# Ensure DL_DIR is absolute and points to the mapped sources directory
 new_lines.append('DL_DIR = "/home/squeezeos/poky/sources"\n')
-# Use the git wrapper to handle file:// protocols
 new_lines.append('GIT = "/home/squeezeos/git_wrapper.sh"\n')
+# Force fuse to use the local mirror
+new_lines.append('PREMIRRORS_append = " http://downloads.sourceforge.net/fuse/.* file:///home/squeezeos/poky/sources/ \\n "\n')
 
 with open(conf_path, 'w') as f:
     f.writelines(new_lines)
 
-print("Successfully updated local.conf with absolute DL_DIR and GIT wrapper")
+print("Successfully updated local.conf with PREMIRRORS for fuse")
