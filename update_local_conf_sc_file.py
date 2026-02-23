2f8ef89c1090727f885f46545d790d73d929ff9f
Igland <igland@gmail.com>
2026-02-23 16:52:09 +0100

    Configure squeezecenter fetch to use local bare clone via file protocol

---
+++ update_local_conf_sc_file.py
@@ -0,0 +1,26 @@
+import os
+
+conf_path = '/home/utking/squeezeos-build/src/build/conf/local.conf'
+
+if not os.path.exists(conf_path):
+    print(f"Error: {conf_path} not found")
+    exit(1)
+
+with open(conf_path, 'r') as f:
+    lines = f.readlines()
+
+new_lines = []
+for line in lines:
+    if 'SQUEEZECENTER_SCM =' in line or \
+       'SQUEEZECENTER_SCM ?=' in line or \
+       'SRCREV_pn-squeezecenter =' in line:
+        continue
+    new_lines.append(line)
+
+new_lines.append('SQUEEZECENTER_SCM = "git:///home/squeezeos/poky/sources/git/github.com.ralph-irving.slimserver.git;protocol=file;branch=public/9.1"\n')
+new_lines.append('SRCREV_pn-squeezecenter = "4967db209310c083db57f70d94e90802d655e973"\n')
+
+with open(conf_path, 'w') as f:
+    f.writelines(new_lines)
+
+print("Successfully injected SQUEEZECENTER_SCM local file protocol override")
